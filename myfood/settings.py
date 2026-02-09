8c7465b8d88315d0e6b921219b020e9d8af61be2
insia <insias83@gmail.com>
2026-02-10 01:46:50 +0300

    Fixed Bad Request 400 error

--- myfood/settings.py
+++ myfood/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wszxigpkcc2%0-bx&(jhd^f99k%vm64fb8e%q)^83urbxp9j3j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
